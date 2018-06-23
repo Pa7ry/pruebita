a24f102ee0696ba20ba9b44ea067b73217c62a23
pa7ry <pa7ry>
2018-06-23 21:44:56 +0200

    app completa

---
+++ pruebita/src/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  user: string;
+  pass: string;
+
+  constructor(public navCtrl: NavController,
+              public toastCtrl: ToastController) {
+
+  }
+
+  access(user, pass) {
+    console.log(this.pass);
+    let boton = document.getElementById('boton');
+    if (this.user == 'Gianmarco' && this.pass == '4mesesdeinfinitos') {
+      boton.disabled = false;
+    }
+  }
+
+  see() {
+    document.getElementById('slides').className = '';
+    console.log('hola');
+    document.getElementById('login').className = 'hide';
+
+    const toast = this.toastCtrl.create({
+      message: '¡TE QUIERO!',
+      duration: 3000
+    });
+    toast.present();
+  }
+
+}
